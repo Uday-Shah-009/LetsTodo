7a873f20a2e76fed31a5bd53fcc89c6f85331865
Uday-Shah-009 <udayshah.24.ce@iite.indusuni.ac.in>
2026-09-22 15:44:18 +0530

    fix: remove logs from deployment

--- src/app/apis/tasks.api.js
+++ src/app/apis/tasks.api.js
@@ -28,7 +28,6 @@ export const createTask = async (payload) => {
 
 export const GetTaskbyId = async (task_id) => {
   const res = await axiosInstance.get(`tasks/${task_id}`);
-  console.log(res.data)
   return res.data;
 };
 
